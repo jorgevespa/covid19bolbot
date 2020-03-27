449641e03bd7d6adba2c6ad4e744040ef76d510e
jorgevespa <46542787+jorgevespa@users.noreply.github.com>
2020-03-27 18:19:50 -0400

    Update covid19bolbot.py

--- covid19bolbot.py
+++ covid19bolbot.py
@@ -101,7 +101,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("1120222946:AAGWMHDgPjthJg81BMCWocDhSJOMCOsBCFI", use_context=True)
+    updater = Updater("TOKEN", use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
